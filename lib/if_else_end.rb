b229e58d8c8c18ba652b714cec9e07229d7955ef
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-05 00:39:22 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,7 @@
 #=> If the current second is an even number, output Even!
 #=> If the current second is an odd number, output Odd!
 
-current_time = Time.now
-current_time = current_time.to_i
- 
-if current_time % 2 == 0
+if Time.now.to_i % 2 == 0
   puts "Even!"
 else
   puts "Odd!"
